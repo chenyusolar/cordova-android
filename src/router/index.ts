5ddee77a9f12d2d08c34aa0c69de3e981ec8c375
chenyusolar <=>
2024-06-17 17:29:27 -0700

    add navbar

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import ChatView from '@/views/ChatView/ChatView.vue'
+import Home from '@/views/home/index.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: Home, 
+    },
+    {
+      path: '/chat',
       name: 'chat',
       component: ChatView,
     }
---
+++ src/views/home/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <t-tab-bar v-model="value" :split="false">
+      <t-tab-bar-item
+        v-for="item in list"
+        :key="item.name"
+        :value="item.name"
+        :badge-props="item.badgeProps"
+        :aria-label="item.ariaLabel"
+      >
+        {{ item.text }}
+        <template #icon>
+          <t-icon :name="item.icon" />
+        </template>
+      </t-tab-bar-item>
+    </t-tab-bar>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  import { Icon as TIcon } from 'tdesign-icons-vue-next';
+  
+  const value = ref('label_1');
+  const list = ref([
+    { name: 'label_1', text: '首页', icon: 'home', badgeProps: { count: 16 }, ariaLabel: '首页，有16条消息' },
+    { name: 'label_2', text: '软件', icon: 'app', badgeProps: { dot: true }, ariaLabel: '软件，有新的消息' },
+    { name: 'label_3', text: '聊天', icon: 'chat', badgeProps: { count: 'New' }, ariaLabel: '聊天，New' },
+    { name: 'label_4', text: '我的', icon: 'user', badgeProps: { count: '···' }, ariaLabel: '我的，有很多消息' },
+  ]);
+  </script>
+  
